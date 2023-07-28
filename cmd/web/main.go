568851c843a0cc928ef8ce7922d2930b093eae03
Vaibhav Pandey <vaibhav.pandey0806@gmail.com>
2023-07-29 04:16:13 +0530

    add self-generated tls-certificate

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"database/sql"
 	"flag"
 	"html/template"
@@ -48,6 +49,7 @@ func main() {
 
 	session := sessions.New([]byte(*secret))
 	session.Lifetime = 12 * time.Hour
+	session.Secure = true
 
 	app := &application{
 		errLog:        errLog,
@@ -57,14 +59,23 @@ func main() {
 		templateCache: templateCache,
 	}
 
+	tlsConfig := &tls.Config {
+		PreferServerCipherSuites: true,
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256}
+	}
+
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errLog,
 		Handler:  app.routes(),
+		TLSConfig: tlsConfig,
+		IdleTimeout: time.Minute,
+		ReadTimeout: 5 * Time.Second,
+		WriteTimeout: 10 * Time.Second,
 	}
 
 	infoLog.Printf("Starting server on port %s\n", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errLog.Fatal(err)
 }
 
